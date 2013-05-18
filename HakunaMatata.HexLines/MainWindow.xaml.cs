a3782d79f928cab8b954e055aaf49e4c7bc3789e
Athari <athari.p@gmail.com>
2013-05-18 05:19:08 +0400

    Separated Ball from Cell

--- HakunaMatata.HexLines/MainWindow.xaml.cs
+++ HakunaMatata.HexLines/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ namespace HakunaMatata.HexLines
         public MainWindow ()
         {
             _table.Resize(16, 10);
+            _table.FillBalls(40);
 
             DataContext = _table;
             InitializeComponent();
@@ -37,16 +38,21 @@ namespace HakunaMatata.HexLines
 
     public class Table
     {
+        public const double CellXOffset = 45;
+        public const double CellYOffset = 52;
+
         private Cell _selectedCell;
 
         public int TableWidth { get; private set; }
         public int TableHeight { get; private set; }
 
         public ObservableCollectionEx<Cell> Cells { get; private set; }
+        public ObservableCollectionEx<Ball> Balls { get; private set; }
 
         public Table ()
         {
             Cells = new ObservableCollectionEx<Cell>();
+            Balls = new ObservableCollectionEx<Ball>();
         }
 
         public Cell SelectedCell
@@ -61,12 +67,12 @@ namespace HakunaMatata.HexLines
 
         public double Width
         {
-            get { return (TableWidth + .5) * Cell.CellXOffset; }
+            get { return (TableWidth + .5) * CellXOffset; }
         }
 
         public double Height
         {
-            get { return (TableHeight + .5) * Cell.CellYOffset; }
+            get { return (TableHeight + .6) * CellYOffset; }
         }
 
         public void Resize (int w, int h)
@@ -74,7 +80,12 @@ namespace HakunaMatata.HexLines
             TableWidth = w;
             TableHeight = h;
             SelectedCell = null;
-            Cells.Replace(Cell.CreateTableCells(TableWidth, TableHeight));
+            Cells.Replace(Cell.GenerateTableCells(TableWidth, TableHeight));
+        }
+
+        public void FillBalls (int count)
+        {
+            Balls.Replace(Ball.GenerateBalls(Cells, TableWidth, TableHeight, count));
         }
 
         private void UpdateSelection (Cell cell, bool value)
@@ -82,7 +93,7 @@ namespace HakunaMatata.HexLines
             if (cell == null)
                 return;
             cell.IsSelected = value;
-            GetNeighbors(cell).ForEach(c => c.IsAvailable = value && !c.HasBall);
+            GetNeighbors(cell).ForEach(c => c.IsAvailable = value && c.Ball == null);
         }
 
         private IEnumerable<Cell> GetNeighbors (Cell cell)
@@ -96,23 +107,32 @@ namespace HakunaMatata.HexLines
             return new[] { n - h + d, n - h + d + 1, n - 1, n + 1, n + h + d, n + h + d + 1 }
                 .Where(i => i >= 0 && i < Cells.Count && Math.Abs(i % h - n % h) <= 1);
         }
+
+        public static double GetCellX (int x, int y)
+        {
+            return x * CellXOffset;
+        }
+
+        public static double GetCellY (int x, int y)
+        {
+            return y * CellYOffset + ((x & 1) != 0 ? CellYOffset / 2 : 0);
+        }
     }
 
     public class Cell : ModelBase<Cell>
     {
-        public const double CellXOffset = 45;
-        public const double CellYOffset = 52;
-
         private bool _isSelected;
         private bool _isAvailable;
 
         public double X { get; private set; }
         public double Y { get; private set; }
-        public bool HasBall { get; private set; }
-        public Color BallColor { get; private set; }
+        public Ball Ball { get; internal set; }
 
-        private Cell ()
-        {}
+        public Cell (double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
 
         public bool IsSelected
         {
@@ -126,27 +146,52 @@ namespace HakunaMatata.HexLines
             set { Set(ref _isAvailable, value); }
         }
 
-        public Brush BallColorBrush
+        public static IEnumerable<Cell> GenerateTableCells (int w, int h)
         {
-            get { return new SolidColorBrush(BallColor); }
-        }
-
-        public static IEnumerable<Cell> CreateTableCells (int w, int h)
-        {
-            var rnd = new Random();
             for (int x = 0; x < w; x++)
                 for (int y = 0; y < h; y++)
-                    yield return new Cell {
-                        X = 30 + x * CellXOffset,
-                        Y = 30 + y * CellYOffset + ((x & 1) != 0 ? CellYOffset / 2 : 0),
-                        BallColor = new Color {
-                            ScR = (float)rnd.NextDouble(),
-                            ScG = (float)rnd.NextDouble(),
-                            ScB = (float)rnd.NextDouble(),
-                            ScA = 255,
-                        },
-                        HasBall = rnd.Next(3) == 0
-                    };
+                    yield return new Cell(Table.GetCellX(x, y) + 30, Table.GetCellY(x, y) + 30);
+        }
+    }
+
+    public class Ball
+    {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+        public Color Color { get; private set; }
+        public Cell Cell { get; private set; }
+
+        private Ball (int x, int y, Cell cell, Color color)
+        {
+            X = Table.GetCellX(x, y) + 10;
+            Y = Table.GetCellY(x, y) + 10;
+            Color = color;
+            Cell = cell;
+            cell.Ball = this;
+        }
+
+        public void MoveTo (Cell cell)
+        {
+            Cell.Ball = null;
+            Cell = cell;
+            Cell.Ball = this;
+        }
+
+        public Brush BallColorBrush
+        {
+            get { return new SolidColorBrush(Color); }
+        }
+
+        public static IEnumerable<Ball> GenerateBalls (IList<Cell> cells, int w, int h, int count)
+        {
+            var rnd = new Random();
+            return Enumerable.Range(0, w * h).OrderBy(i => rnd.Next()).Take(count)
+                .Select(ic => new Ball(ic / h, ic % h, cells[ic], new Color {
+                    ScR = (float)rnd.NextDouble(),
+                    ScG = (float)rnd.NextDouble(),
+                    ScB = (float)rnd.NextDouble(),
+                    ScA = 255,
+                }));
         }
     }
 }
